04e827b8c7e6bc49739ea8163826d750c741ee46
antonixe <khaaoyatoony@gmail.com>
2023-05-23 00:48:12 -0700

    extra

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3
 """
-a module that queries the Reddit API and returns a list
-containing the titles of all hot articles
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
 """
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """
-    a function that that queries the Reddit API and returns
-    a list containing the titles of all hot article
-    """
-    if hot_list is None:
-        hot_list = []
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            hot_list.append(post['data']['title'])
-        after = data['data']['after']
-        if after is not None:
-            recurse(subreddit, hot_list, after)
-        return hot_list
-    elif response.status_code == 404:
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
         return None
-    else:
-        raise ValueError(f"Request failed with status code\
-                         {response.status_code}")
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
